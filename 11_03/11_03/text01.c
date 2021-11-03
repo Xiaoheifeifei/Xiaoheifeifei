e5b08efc7e6a91a19143cb3255b39631c5b81b8f
Fei_fei <93477424+Xiaoheifeifei@users.noreply.github.com>
2021-11-03 23:44:55 +0800

    Add files via upload

---
+++ 11_03/11_03/text01.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+//int main()
+//{
+	//printf("c:\code\test.c");
+	//printf("xiaohei\nxiaohei");
+	//printf("abc\0der");
+	//printf("are you ok\?\?");
+	//printf("%s\n", "\"abc\"");
+	//printf("%c\n",'\'');
+	
+	//return 0;
+//}
+//int main()
+//{
+//	//printf("%c\n", '\117');//八进制130转换成十进制是多少呢？
+//	//117=8^2+8^1+7=79=O
+//	printf("%c\n", '\x35');//十六进制35转换成十进制是多少呢？
+//
+//	return 0;
+//}
+#include <string.h>
+//int main()
+//{
+//	int input = 0;
+//	printf("加入我\n要好好学习嘛?(1/0)");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("拿到好offer");
+//	else
+//		printf("卖红薯");
+//	return 0;
+//}
+int main()
+{
+	/*int line = 0;
+	while (line < 30000)
+	{
+		printf("敲代码:%d\n", line);
+		line++;
+	}
+	printf("拿到好offer");*/
+	
+	return 0;
+}
+
