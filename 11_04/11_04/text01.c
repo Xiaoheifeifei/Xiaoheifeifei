e165328e2dc645017bf7785677bd81424defa8cd
Fei_fei <93477424+Xiaoheifeifei@users.noreply.github.com>
2021-11-04 23:36:00 +0800

    Add files via upload

---
+++ 11_04/11_04/text01.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+//int Add(int x,int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	//int num1 = 0;
+//	//int num2 = 0;
+//	//scanf("%d%d",&num1,& num2);
+//	////int sum = num1 + num2;
+//	//int sum = Add(num1, num2);
+//	//printf("sum=%d\n", sum);
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d ", arr[i]);
+//		i++;
+//	}
+//	//return 0;
+
+//int main()
+//{
+//	//double a = 9 / 2.0;
+//
+//	//printf("%lf\n", a);
+//	int a = 8 % 3;
+//	printf("%d", a);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 5;
+//		printf("%d\n", !a);
+//		if (a)
+//		{
+//
+//	}
+//		if (!a)
+//		{
+//
+//		}
+//		return 0;
+//}
+//
+int main()
+{
+	int a = 0;
+	printf("%d\n", ~a);
+	return 0;
+}
