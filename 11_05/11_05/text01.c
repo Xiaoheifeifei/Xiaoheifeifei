0275b445eb910cc07e7090c692b42ea1838c5dd7
Fei_fei <93477424+Xiaoheifeifei@users.noreply.github.com>
2021-11-05 23:35:13 +0800

    Add files via upload

---
+++ 11_05/11_05/text01.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = ++a;//前置++ -先++，后使用
+//	printf("%d\n", b);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = a++; //后置++ -先使用，后++
+//	printf("%d\n", b);
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = (int)3.14;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//逻辑或
+//	//int a = 3;//||一真即为真
+//	int a = 0;//两假即为假
+//	int b = 0;
+//	//int b = 5;//||两真即为真
+//	int c = a || b;
+//	//逻辑与
+//	//int b = 5;//两真即为真
+//	//int b = 0;//&&两边有一假即为假
+//	//int a = 0;
+//	//int c = a && b;
+//	printf("%d\n", c);
+//	/*if (a && b)
+//	{
+//
+//	}*/
+//	
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 5;
+//	int b = 5;
+//	int max = 0;
+//	/*if (a > b)
+//		max = a;
+//	else
+//		max = b;*/
+//	max = a > b ? a : b;
+//	printf("%d\n", max);
+//	return 0;
+//}
+//
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%d\n", arr[4]);
+	return 0;
+}
