eff02075a60bf96ab615ccaf7974028027df4fe1
Fei_fei <93477424+Xiaoheifeifei@users.noreply.github.com>
2021-11-06 23:56:16 +0800

    Add files via upload

---
+++ 11_06/11_06/text01.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+////
+////
+////
+////typedef unsigned int u_int;
+////int main()
+////{
+////	unsigned int num = 100;
+////	u_int num = 100;
+////	return 0;
+////}
+//void test()
+//{
+//	static int a = 1;//2,3,4,5,6,7,8,9,10,11
+//	a++;
+//	printf("%d", a);
+//	
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//
+//	return 0;
+//}
+//
+//extern int Add(int, int);
+//int main()
+//{
+//	int a = 30;
+//	int b = 20;
+//	int sum = Add(a, b);
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+//define是一个预处理指令
+//1.define定义符号
+//#define MAX 1000
+//int main()
+//{
+//	
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+//2.define 定义宏
+//#define ADD(x,y) ((x)+(y))
+//int main()
+//{
+//	printf("%d\n", 4*ADD(5,6));
+//	return 0;
+//}
