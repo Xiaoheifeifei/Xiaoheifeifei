88f6f1dd58dea18eccff9cad6205c41d2c7e689b
Fei_fei <93477424+Xiaoheifeifei@users.noreply.github.com>
2021-11-13 23:17:03 +0800

    Add files via upload

---
+++ 11_13/11_13/test.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//			//break;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+////循环体内改变循环变量的例子
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++ )
+//	{
+//
+//		printf("%d ", i);
+//		//i++;
+//		int j = 0;
+//		for (j = 1; j < 10; i++)
+//		{
+//			printf("xiaohei\n");
+//		}
+//		//i = 5;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	//判断部分省略-判断部分恒为真-死循环
+//	for (; ;)
+//	{
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int  i = 0;
+//	int j = 0;
+//	for (; i < 5; i++)
+//	{
+//		for (; j < 4; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//int x, y;
+//for (x = 0, y = 0; x < 2 && y < 5; ++x, y++)
+//{
+//	printf("hehe\n");
+//}
+//return 0;
+//}
