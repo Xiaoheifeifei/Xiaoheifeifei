7833156732c9405dfbe2e1fedf4fdb1847cebd3c
Fei_fei <93477424+Xiaoheifeifei@users.noreply.github.com>
2021-11-18 21:24:01 +0800

    Add files via upload

---
+++ 11_18/11_18/test.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+//int main()
+//{
+//	printf("%d", 15 + 37);
+// 
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("15减去37的结果是%d。\n", 15 - 37);
+//
+//	return 0;
+//}
+
+//if () while() Function(x, y, z)
+
+//if (condition)
+//{
+//	// program code
+//}
+//else
+//{
+//	// program code
+//}
+
+//void Function(int x)
+//{
+//	// program code
+//}
+
+//for (initialization; condition; update)
+//{
+//	// program code
+//}
+//
+//while (condition)
+//{
+//	// program code
+//}
+//
+//char *name;
+//int *x, y;
+//int* x, y;
+
+int main()
+{
+	printf("hello, I'm feifei.\n");
+
+	return 0;
+}
