0704551ca77ad21422dec50258d2d0741c7a9ebd
Fei_fei <93477424+Xiaoheifeifei@users.noreply.github.com>
2021-11-20 22:29:59 +0800

    Add files via upload

---
+++ 11_20/test.c
@@ -0,0 +1,140 @@
+ #define _CRT_SECURE_NO_WARNINGS 1 
+ #include <stdio.h>
+// int main()
+// {
+//     printf("你好!\a\a\a\n");
+//     printf("天\n地\n人\n");
+//     printf("喂！\n\n你好！\n再见。\n");
+//     int vx, vy;
+//     vx = 57;
+//     vy = vx + 10;
+//     int vx = 57;
+//     int vy = vx + 10;
+//     printf("vx的值是%d。\n",vx);
+//     printf("vy的值是%d。\n",vy);
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x = 3.14;
+//     int y = x + 5.7;
+//     printf("x的数值是%d\n",x);
+//     printf("y的数值是%d\n",y);
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     int no;
+//     printf("请输入一个整数:>");
+//     scanf("%d",&no);
+//     printf("你输入的是:%d\n",no);
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     int a = 0;
+//     printf("你想做什么？(1/0)\n");
+// scanf("%d",&a);
+//  while(1)
+//  {
+//         if(a == 1 )
+// {
+//     printf("学习\n");
+//     break;
+// }
+// else
+// {
+//     printf("努力学习\n");
+//     break;
+// }
+//  }
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     int no;
+//     printf("请输入一个整数:>");
+//     scanf("%d",&no);
+
+//     printf("它的五倍数是：%d\n",5*no);
+
+//     return 0;
+// }
+
+// int main(void)
+// {
+// int no;
+// printf("请输入一个整数：");
+// scanf("%d",&no);
+// printf("该整数加上12的结果是%d。\n",no + 12);
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     int x, y;
+//     puts("请输入两个整数。");
+//     printf("整数1：");
+//     scanf("%d",&x);
+//     printf("整数2：");
+//     scanf("%d",&y);
+//     printf("它们的和是%d。\n",x + y);
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     int x, y;
+//     int sum;
+//     puts("请输入两个整数。");
+//     printf("整数1：");
+//     scanf("%d",&x);
+//     printf("整数2：");
+//     scanf("%d",&y);
+//     sum = x + y;
+//     printf("它们的和是%d。\n",sum);
+//     return 0;
+// }
+
+// int main(void)
+// {
+// puts("天");
+// puts("地");
+// puts("人");
+//     return 0;
+// }
+
+// int main(void)
+// {
+//     int x, y;
+//     int z;
+//     puts("请输入两个整数。");
+// printf("整数1：");
+// scanf("%d",&x);
+// printf("整数2：");
+// scanf("%d",&y);
+// z = x*y;
+// printf("它们的乘积是%d。\n",z);
+//     return 0;
+// }
+
+int main(void)
+{
+    int x, y, z;
+    int m;
+    puts("请输入三个整数。");
+printf("整数1：");
+scanf("%d",&x);
+printf("整数2：");
+scanf("%d",&y);
+printf("整数3：");
+scanf("%d",&z);
+m = x+y+z;
+printf("它们的和是%d。\n",m);
+    return 0;
+}
