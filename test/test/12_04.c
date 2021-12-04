01db65a4d6640eede10496dc95f0f9430c844db7
Fei_fei <93477424+Xiaoheifeifei@users.noreply.github.com>
2021-12-05 00:04:37 +0800

    Add files via upload

---
+++ test/test/12_04.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+//int main(void)
+//{
+//	int no;
+//	printf("正整数：");
+//	scanf("%d", &no);
+//	int i = 0;
+//	while (i < no) {
+//		if (no % 2)
+//			printf("+");
+//		else
+//			printf("-");
+//		no--;
+//	}
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int no;
+//	printf("正整数：");
+//	scanf("%d", &no);
+//	int i = 1;
+//	while (i <= no) {
+//		if (i % 2)
+//			printf("+");
+//		else
+//			printf("-");
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	int num, i;
+//	printf("要输入多少个整数: ");
+//	scanf("%d", &num);
+//	i = 0;
+//	while (num >= 0 && i < num) {
+//		if (i % 2)
+//			putchar('-');
+//		else
+//			putchar('+');
+//				i++;
+//	}
+//	return 0;
+//	}
